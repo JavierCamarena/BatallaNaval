9a53d0ff18a19fa2e4c2b56e63890e89e4855fe9
JavierCamarena <j4vi3i2@gmail.com>
2016-06-02 20:44:25 -0500

    Inicio de proyecto

---
+++ src/Objects/Map.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+
+/**
+ *
+ * @author xaovs
+ */
+public class Map implements Objeto{
+    private final int SIZE      = 10;
+    public final int WATER      = 1;
+    public final int SHIP       = 2;
+    public final int XPLOTION   = 3;
+    
+    private int [][] world;
+    
+    public Map(){
+        initWorld();
+    }
+    
+    public void initWorld(){
+        world = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                world[i][j] = WATER; 
+            }
+        }
+    }
+
+    @Override
+    public void paint() {
+        System.out.println("Pintando Mapa");
+    }
+    
+}
---
+++ src/Objects/Objeto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+
+/**
+ *
+ * @author xaovs
+ */
+public interface Objeto {
+    public void paint();
+}
